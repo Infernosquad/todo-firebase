4e48e77aba96eebaf8bcc88c9ebf0c29e6ad4919
infernosquad <infzanoza@gmail.com>
2021-02-24 00:17:25 -0800

    Add prod credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBIuZSX3Y5ZEUzKUfCq2aLvur6weuoErZc',
+    authDomain: 'todo-fir.firebaseapp.com',
+    projectId: 'todo-fir',
+    storageBucket: 'todo-fir.appspot.com',
+    messagingSenderId: '373613315156',
+    appId: '1:373613315156:web:57191c6d6ad8e3ee4cd9c1'
+  }
 };
